e86f858a8473001e92c35e74d4c11120b2ec0448
DY-Tempus <2744ch@naver.com>
2025-05-12 14:43:11 +0900

    init

---
+++ src/main/java/com/grepp/codemap/CodeMapApplication.java
@@ -0,0 +1,13 @@
+package com.grepp.codemap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeMapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeMapApplication.class, args);
+    }
+
+}
