47bcc03ec3fec4960c6d1d576757bd037b2c9033
yoozafree <tjdbs929@gmail.com>
2025-05-15 17:12:57 +0900

    refactor : User 수정 + signup.html 수정

--- src/main/java/com/grepp/codemap/user/controller/UserController.java
+++ src/main/java/com/grepp/codemap/user/controller/UserController.java
@@ -42,6 +42,8 @@ public class UserController {
             BindingResult bindingResult,
             Model model){
 
+        log.info("폼 데이터: {}", form);
+
         if(bindingResult.hasErrors()){
             return "user/signup";
         }
