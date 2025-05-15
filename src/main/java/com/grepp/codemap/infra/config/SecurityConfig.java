f60a383c5c3ecbbc741b2512d80508a56b0d6828
yoozafree <tjdbs929@gmail.com>
2025-05-15 16:05:17 +0900

    feat : 로그인 + 회원가입

--- src/main/java/com/grepp/codemap/infra/config/SecurityConfig.java
+++ src/main/java/com/grepp/codemap/infra/config/SecurityConfig.java
@@ -16,7 +16,9 @@ import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
 import java.io.IOException;
+
 import static org.springframework.http.HttpMethod.GET;
 import static org.springframework.http.HttpMethod.POST;
 
