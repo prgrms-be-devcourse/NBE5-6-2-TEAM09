d7035dc5bc0de63d5071bb39399c345b153aed88
yoozafree <tjdbs929@gmail.com>
2025-06-17 16:51:18 +0900

    fix: interview KeyCompareService + etc

--- src/main/java/com/grepp/codemap/interview/repository/UserAnswerRepository.java
+++ src/main/java/com/grepp/codemap/interview/repository/UserAnswerRepository.java
@@ -3,6 +3,7 @@ package com.grepp.codemap.interview.repository;
 
 import com.grepp.codemap.interview.domain.InterviewQuestion;
 import com.grepp.codemap.interview.domain.UserAnswer;
+import com.grepp.codemap.interview.dto.QuestionSummaryDto;
 import com.grepp.codemap.user.domain.User;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -23,13 +24,24 @@ public interface UserAnswerRepository extends JpaRepository<UserAnswer, Long> {
     @EntityGraph(attributePaths = "question")
     List<UserAnswer> findByUserIdOrderByIdDesc(Long userId);
 
-    @Query("SELECT ua FROM UserAnswer ua " +
-            "WHERE ua.id IN (" +
-            "   SELECT MAX(uaSub.id) FROM UserAnswer uaSub " +
-            "   WHERE uaSub.user = :user " +
-            "   GROUP BY uaSub.question" +
-            ")")
-    List<UserAnswer> findLatestAnswersByUser(@Param("user") User user);
+    @Query("""
+                SELECT new com.grepp.codemap.interview.dto.QuestionSummaryDto(
+                    ua.question.id,
+                    ua.question.questionText,
+                    ua.question.category,
+                    ua.question.difficulty
+                )
+                FROM UserAnswer ua
+                WHERE ua.user.id = :userId
+                  AND ua.answeredAt = (
+                      SELECT MAX(subUa.answeredAt)
+                      FROM UserAnswer subUa
+                      WHERE subUa.user.id = :userId
+                        AND subUa.question.id = ua.question.id
+                  )
+            """)
+    List<QuestionSummaryDto> findLatestAnswersByUserId(@Param("userId") Long userId);
+
 
 
 }
